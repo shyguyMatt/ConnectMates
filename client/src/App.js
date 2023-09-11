b23c8d5e48301695251f803de9b7c6c1ca02375e
ShyguyMatt <mjherford@gmail.com>
2023-09-11 19:42:46 -0400

    added a modal which can load content to it

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import React, { useState } from 'react';
 import Header from './componants/elements/Header';
 import Footer from './componants/elements/Footer';
+import Modal from './componants/elements/Modal';
+
 import Home from './componants/pages/Home';
 import Profile from './componants/pages/Profile';
 
 export default function App() {
   const [currentPage, setCurrentPage] = useState('Home');
+  const [modal, setModal] = useState(true);
 
   const renderPage = () => {
     if (currentPage === 'Home') {
@@ -17,10 +20,21 @@ export default function App() {
     return <Home />;
   }
 
+  const handleModal = () => setModal(false)
+  
+  const renderModal = () => {
+    if (modal) {
+      return <Modal handleModal={handleModal}/>
+    }
+    return;
+  }
+
   const handlePageChange = (page) => setCurrentPage(page);
+
 
   return (
     <div>
+      {renderModal()}
       <Header />
       {renderPage()}
       <Footer />

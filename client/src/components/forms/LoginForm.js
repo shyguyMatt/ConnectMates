ae46ba22e765235dbc7cefc9c297f7667186b07d
ShyguyMatt <mjherford@gmail.com>
2023-09-12 17:55:54 -0400

    login form accepts and clears info

--- client/src/components/forms/LoginForm.js
+++ client/src/components/forms/LoginForm.js
@@ -1,14 +1,66 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { useMutation } from '@apollo/client';
+// import { LOGIN_USER } from '../../utils/mutations';
+
+import Auth from '../../utils/auth'
 
 export default function LoginForm() {
+  
+const [formState, setFormState] = useState({email: '', password: ''});
+// const [login, { error, data }] = useMutation(LOGIN_USER);
+
+const handleChange = (event) => {
+  const { name, value } = event.target;
+
+  setFormState({
+    ...formState,
+    [name]: value,
+  });
+};
+
+// *MUTATION STILL NEEDS TO BE MADE*
+const handleFormSubmit = async (e) => {
+  e.preventDefault();
+  
+  // try {
+  //   const { data } =  await login({
+  //     variables: { ...formState },
+  //   });
+
+  //   Auth.login(data.login.token);
+  // } catch (error) {
+  //   console.error(error);
+  // }
+
+  setFormState({
+    email: '',
+    password: '',
+  });
+};
+
   return(
-    <form>
+    <form onSubmit={handleFormSubmit}>
+
       <h2>Email:</h2>
-      <input className='email' placeholder='Email' />
-      <h2>Name:</h2>
-      <input className='name' placeholder='Name' />
+      <input
+      className='form-input'
+      placeholder='Email'
+      name='email'
+      type='email'
+      value={formState.email}
+      onChange={handleChange}
+      />
+
       <h2>Password:</h2>
-      <input className='password' placeholder='Password' />
+      <input
+      className='password'
+      placeholder='Password'
+      name='password'
+      type='password'
+      value={formState.password}
+      onChange={handleChange}
+      />
+
       <button type='submit'>Log in</button>
     </form>
   )
--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,1 +1,3 @@
 import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql``;

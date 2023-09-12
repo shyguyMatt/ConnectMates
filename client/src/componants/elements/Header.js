c46ad15d4ed8112cbbf82c1a160f1e4ea5c05947
ShyguyMatt <mjherford@gmail.com>
2023-09-11 21:52:17 -0400

    fixed errors preventing page from loading

--- client/src/componants/elements/Header.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Header() {
-  return(
-    <header>
-      <p>This is the header</p>
-    </header>
-  )
-}
--- client/src/componants/pages/Home.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Home() {
-  return(
-    <div className='home'>
-      <p>This is the home page</p>
-    </div>
-  )
-}

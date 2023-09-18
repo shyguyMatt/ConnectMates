9f5ae22e053daaa6580b6e4cb59060b841bdc3f6
ShyguyMatt <mjherford@gmail.com>
2023-09-18 14:16:36 -0400

    added distinction between admin and member groups

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -3,7 +3,6 @@ import { useQuery } from '@apollo/client';
 import { QUERY_SINGLE_USER, QUERY_USERS_GROUPS } from '../../utils/queries';
 import Auth from './../../utils/auth'
 
-// Import forms
 import NewGroup from './../elements/NewGroup'
 
 import './../../styles/home.css';
@@ -29,25 +28,34 @@ export default function Home() {
       <div>Loading...</div>
     )
   }
+
   if(loadingUser) {
     return(
       <div>Loading...</div>
     )
   }
-  // console.log(user)
 
   return(
     <div className='homeContainer'>
-      <div className='groups'>
-        <h2>These are your groups</h2>
+
+      <div className='adminGroups'>
+        <h2>Groups you are admin of:</h2>
         {adminGroups.map((group) => (
           <div key={group._id}>
             <h2>this is am admin group</h2>
           </div>
           ))}
-        <NewGroup user={user}/>
       </div>
 
+      <div className='memberGroups'>
+        <h2>Groups you are a member in:</h2>
+        {memberGroups.map((group) => {
+          <div key={group._id}>
+            <h2>this is a member group</h2>
+          </div>
+        })}
+      </div>
+        <NewGroup user={user}/>
     </div>
   )
 };

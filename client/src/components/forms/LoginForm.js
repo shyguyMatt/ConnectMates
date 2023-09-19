f85f8767338ef685e04f9c11bd2b88628aa78e4e
kevhuff <kevinhuffman23@gmail.com>
2023-09-18 20:36:10 -0400

    adjusted style of profile page

--- client/src/components/forms/LoginForm.js
+++ client/src/components/forms/LoginForm.js
@@ -1,72 +1,87 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { useMutation } from '@apollo/client';
 import { LOGIN_USER } from '../../utils/mutations';
-
-import Auth from '../../utils/auth'
+import Auth from '../../utils/auth';
 
 export default function LoginForm() {
-// Define states to be used
-const [formState, setFormState] = useState({email: '', password: ''});
+  const [formState, setFormState] = useState({ email: '', password: '' });
+  const [login, { error, data }] = useMutation(LOGIN_USER);
 
-// define mutations to be used
-const [login, { error, data }] = useMutation(LOGIN_USER);
-
-
-// handles any change in the fields and updates states accordingly
-const handleChange = (event) => {
-  const { name, value } = event.target;
-
-  setFormState({
-    ...formState,
-    [name]: value,
-  });
-};
-
-// handles the form submit
-const handleFormSubmit = async (e) => {
-  e.preventDefault();
-  
-  try {
-    const { data } =  await login({
-      variables: { ...formState },
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormState({
+      ...formState,
+      [name]: value,
     });
+  };
 
-    Auth.login(data.login.token);
-    
-  } catch (error) {
-    console.error(error);
-  }
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await login({
+        variables: { ...formState },
+      });
+      Auth.login(data.login.token);
+    } catch (error) {
+      console.error(error);
+    }
+    setFormState({
+      email: '',
+      password: '',
+    });
+  };
 
-  setFormState({
-    email: '',
-    password: '',
-  });
-};
-
-  return(
-    <form onSubmit={handleFormSubmit}>
-
-      <h2>Email:</h2>
-      <input
-      className='form-input'
-      placeholder='Email'
-      name='email'
-      type='email'
-      value={formState.email}
-      onChange={handleChange}
-      />
-
-      <h2>Password:</h2>
-      <input
-      className='password'
-      placeholder='Password'
-      name='password'
-      type='password'
-      value={formState.password}
-      onChange={handleChange}
-      />
-
-      <button type='submit'>Log in</button>
-    </form>
-  )
+  return (
+    <div className=" flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-md w- space-y-4 bg-red-600 p-6 rounded shadow-lg">
+        <div>
+          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+            Sign in to your account
+          </h2>
+        </div>
+        <form onSubmit={handleFormSubmit} className="mt-8 space-y-6">
+          <input type="hidden" name="remember" value="true" />
+          <div className="rounded-md shadow-sm -space-y-px">
+            <div>
+              <input
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                placeholder="Email address"
+                name="email"
+                type="email"
+                required
+                value={formState.email}
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              <input
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                placeholder="Password"
+                name="password"
+                type="password"
+                required
+                value={formState.password}
+                onChange={handleChange}
+              />
+            </div>
+          </div>
+          <div className="flex items-center justify-between">
+            <div className="text-sm">
+              <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
+                Forgot your password?
+              </a>
+            </div>
+          </div>
+          <div>
+            <button type="submit" className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+              Log in
+            </button>
+            <button type="button" className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-green-500 hover:bg-green-600 mt-4 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500">
+              Sign Up
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }

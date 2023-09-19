c9e24c67d66cb4308dafa969d1295ea1154d5bfe
ShyguyMatt <104956465+shyguyMatt@users.noreply.github.com>
2023-09-19 19:47:59 -0400

    Update connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/connectMates');
+// updated mongo atlas uri
 
 module.exports = mongoose.connection;
 

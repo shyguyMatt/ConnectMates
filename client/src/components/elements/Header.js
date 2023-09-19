ea12979377ba4706159787f6a33672cb410eecd8
Michael Fowler <redfaction8008@gmail.com>
2023-09-18 21:22:49 -0500

    Header css bio button edit added groups section in section2

--- client/src/components/elements/Header.js
+++ client/src/components/elements/Header.js
@@ -1,12 +1,13 @@
 import React from 'react';
-
 import photo from '../../assets/images/header.png';
 
 
 export default function Header() {
   return (
-    <header>
-      <img className="headerImg" src={photo} alt="grass with logo text" width="1024" height="350" />
-    </header>
+    <header className="flex items-center justify-center space-x-4">
+    <img src={require('./../../assets/images/torch.gif')} alt="Torch" className="mx-4" />
+    <img className="headerImg mx-auto" src={photo} alt="grass with logo text" width="1024" height="350" />
+    <img src={require('./../../assets/images/torch.gif')} alt="Torch" className="mx-4" />
+  </header>
   )
 }
